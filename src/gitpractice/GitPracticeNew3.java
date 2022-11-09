f5bc045c15cecc3b6ee623d9c636e8f35ba0e733
kumar nagarajan <kumar.nagarajan@tekschool.us>
2022-11-09 15:03:46 -0800

    first commit

---
+++ src/gitpractice/GitPracticeNew3.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class GitPracticeNew3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi this git testing");
+	}
+
+}
